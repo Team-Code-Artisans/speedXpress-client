95abb8db507a964a89da82edd45379c779ee32c3
Abu Taher <writerabutaher@gmail.com>
2023-05-26 22:59:26 +0600

    line added

--- src/pages/home/service/Service.jsx
+++ src/pages/home/service/Service.jsx
@@ -1,7 +1,11 @@
 const Service = () => {
     return (
         <div>
-           <h2>this is service page</h2> 
+            <h2>this is service page</h2>
+            <h2>this is service page</h2>
+            <h2>this is service page</h2>
+            <h2>this is service page</h2>
+            <h2>this is service page</h2>
         </div>
     );
 };
