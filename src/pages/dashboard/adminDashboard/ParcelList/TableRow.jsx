f037686d5ead83b7ce3c6af3ee44e33ceab9c2ac
Tofayel <mdtofayelhossain69@gmail.com>
2023-05-29 22:49:35 +0600

    all parcel table added and bug fix

--- src/pages/dashboard/adminDashboard/ParcelList/TableRow.jsx
+++ src/pages/dashboard/adminDashboard/ParcelList/TableRow.jsx
@@ -56,6 +56,11 @@ const TableRow = () => {
                         <GiHamburgerMenu onClick={myFun}></GiHamburgerMenu>
                     </p>
                 </td>
+                <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                    <p className="text-gray-900 whitespace-no-wrap">
+                        Omuk ahamed 
+                    </p>
+                </td>
             
             </tr>
         </>
