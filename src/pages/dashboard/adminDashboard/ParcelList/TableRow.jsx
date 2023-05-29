d1ad30bd29d88b7c652e818328cd9f8ede37bd1b
ashikur-rahman <ashikur35-540@diu.edu.bd>
2023-05-29 22:48:18 +0600

    all marhchants added✌

--- src/pages/dashboard/adminDashboard/ParcelList/TableRow.jsx
+++ src/pages/dashboard/adminDashboard/ParcelList/TableRow.jsx
@@ -10,7 +10,7 @@ const TableRow = () => {
     }
     return (
         <>
-               <tr>
+            <tr>
                 <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
                     <div className="flex items-center">
                         <div className="flex-shrink-0 w-10 h-10">
@@ -18,12 +18,12 @@ const TableRow = () => {
                                 src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.2&w=160&h=160&q=80"
                                 alt="" />
                         </div>
-                            <div className="ml-3">
-                                <p className="text-gray-900 whitespace-no-wrap">
-                                    Vera Carpenter
-                                </p>
-                            </div>
+                        <div className="ml-3">
+                            <p className="text-gray-900 whitespace-no-wrap">
+                                Vera Carpenter
+                            </p>
                         </div>
+                    </div>
                 </td>
                 <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
                     <p className="text-gray-900 whitespace-no-wrap">Cloth</p>
@@ -43,7 +43,7 @@ const TableRow = () => {
                         className="relative inline-block px-3 py-1 font-semibold text-green-900 leading-tight">
                         <span aria-hidden
                             className="absolute inset-0 bg-green-200 opacity-50 rounded-full"></span>
-                    <span className="relative">Painding</span>
+                        <span className="relative">Painding</span>
                     </span>
                 </td>
                 <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
@@ -56,7 +56,7 @@ const TableRow = () => {
                         <GiHamburgerMenu onClick={myFun}></GiHamburgerMenu>
                     </p>
                 </td>
-            
+
             </tr>
         </>
     );
