799afd6b11662c76218a591f534d006b10667f2f
Abu Taher <writerabutaher@gmail.com>
2023-05-29 22:46:13 +0600

    stats

---
+++ src/pages/dashboard/adminDashboard/AdminHome/AdminHome.jsx
@@ -0,0 +1,11 @@
+import Stats from "./Stats";
+
+const AdminHome = () => {
+    return (
+        <div className=' max-w-screen-2xl mx-auto py-10'>
+            <Stats />
+        </div>
+    );
+};
+
+export default AdminHome;
