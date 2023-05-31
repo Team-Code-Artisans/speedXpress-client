c47f332f3df6315a2cce02267521d61962692ee9
Abu Taher <writerabutaher@gmail.com>
2023-05-31 17:51:46 +0600

    Line Chart

--- src/pages/dashboard/adminDashboard/AdminHome/AdminHome.jsx
+++ src/pages/dashboard/adminDashboard/AdminHome/AdminHome.jsx
@@ -5,7 +5,7 @@ const AdminHome = () => {
     return (
         <div className='max-w-screen-2xl mx-auto py-10'>
             <Stats />
-            {/* <LineChart /> */}
+            <LineChart />
         </div>
     );
 };
--- src/pages/dashboard/adminDashboard/AdminHome/LineChart.jsx
+++ src/pages/dashboard/adminDashboard/AdminHome/LineChart.jsx
@@ -1,28 +1,41 @@
 import React from 'react'
 import { Line } from 'react-chartjs-2';
+import { Chart as ChartJS, LineElement, CategoryScale, LinearScale, PointElement, Tooltip, Legend } from 'chart.js'
+
+ChartJS.register(
+    LineElement, CategoryScale, LinearScale, PointElement, Legend, Tooltip
+)
 
 const LineChart = () => {
 
-    const labels = ['January', 'February', 'March', 'April', 'May', 'June']
     const data = {
-        labels: labels,
+        labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
         datasets: [{
-            label: 'My First Dataset',
+            label: 'Showing Monthly Order',
             data: [65, 59, 80, 81, 56, 55, 40],
             fill: false,
-            borderColor: 'rgb(75, 192, 192)',
+            borderColor: 'rgb(244, 91, 29)',
+            pointBorderColor: 'rgb(244, 91, 29)',
             tension: 0.1
         }]
-    };
+    }
 
-    const config = {
-        type: 'line',
-        data: data,
+    const options = {
+        Plugin: {
+            legend: true,
+            Tooltip: true
+        },
+        scales: {
+            y: {
+                min: 10,
+                max: 100,
+            }
+        }
     };
 
     return (
-        <div>
-            <Line data={data} />
+        <div className='md:w-[40rem] py-10'>
+            <Line data={data} options={options} />
         </div>
     )
 }
