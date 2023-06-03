c8db9748497edd01eea79bde2aeeda38a18cc59f
Abu Taher <writerabutaher@gmail.com>
2023-06-03 20:26:21 +0600

    features

--- src/pages/About/AboutUs.jsx
+++ src/pages/About/AboutUs.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
-
+import Logistics from '../../Assets/Logistics-amico.png'
 
 const AboutUs = () => {
     return (
-        <div>
+        <div className='py-20'>
 
-            <div class="py-16">
+            <div class="pb-20">
                 <div class="xl:container m-auto px-6 text-gray-600 md:px-12 xl:px-16">
                     <div class="lg:bg-gray-50 dark:lg:bg-darker lg:p-16 rounded-[4rem] space-y-6 md:flex flex-row-reverse md:gap-6 justify-center md:space-y-0 lg:items-center">
                         <div class="md:5/12 lg:w-1/2">
                             <img
-                                src="images/pie.svg"
+                                src={Logistics}
                                 alt="image"
                                 loading="lazy"
                                 width=""
@@ -18,7 +18,7 @@ const AboutUs = () => {
                             />
                         </div>
                         <div class="md:7/12 lg:w-1/2">
-                            <h2 class="text-3xl font-bold text-gray-900 md:text-4xl dark:text-white">
+                            <h2 class="text-3xl font-bold text-gray-900 md:text-4xl dark">
                                 Create an efficient and user-friendly platform for managing logistics operations.
                             </h2>
                             <p class="my-8 text-gray-600 dark:text-gray-300">
@@ -26,13 +26,13 @@ const AboutUs = () => {
                             </p>
                             <div class="divide-y space-y-4 divide-gray-100 dark:divide-gray-800">
                                 <div class="mt-8 flex gap-4 md:items-center">
-                                    <div class="w-12 h-12 flex gap-4 rounded-full bg-indigo-100 dark:bg-indigo-900/20">
-                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6 m-auto text-indigo-500 dark:text-indigo-400">
+                                    <div class="w-12 h-12 flex gap-4 rounded-full bg-orange-100 dark:bg-orange-900/20">
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6 m-auto text-orange-500 dark:text-orange-400">
                                             <path fill-rule="evenodd" d="M4.848 2.771A49.144 49.144 0 0112 2.25c2.43 0 4.817.178 7.152.52 1.978.292 3.348 2.024 3.348 3.97v6.02c0 1.946-1.37 3.678-3.348 3.97a48.901 48.901 0 01-3.476.383.39.39 0 00-.297.17l-2.755 4.133a.75.75 0 01-1.248 0l-2.755-4.133a.39.39 0 00-.297-.17 48.9 48.9 0 01-3.476-.384c-1.978-.29-3.348-2.024-3.348-3.97V6.741c0-1.946 1.37-3.68 3.348-3.97zM6.75 8.25a.75.75 0 01.75-.75h9a.75.75 0 010 1.5h-9a.75.75 0 01-.75-.75zm.75 2.25a.75.75 0 000 1.5H12a.75.75 0 000-1.5H7.5z" clip-rule="evenodd" />
                                         </svg>
                                     </div>
                                     <div class="w-5/6">
-                                        <h4 class="font-semibold text-lg text-gray-700 dark:text-indigo-300">Automated Notifications</h4>
+                                        <h4 class="font-semibold text-lg text-gray-700 dark:text-orange-300">Automated Notifications</h4>
                                         <p class="text-gray-500 dark:text-gray-400">Automated notifications and updates regarding shipment status</p>
                                     </div>
                                 </div>
@@ -52,6 +52,68 @@ const AboutUs = () => {
                     </div>
                 </div>
             </div>
+
+            <section className="bg-gray-50 rounded-[4rem] text-gray-700 py-12 lg:mx-16 px-4">
+                <div tabIndex={0} aria-label="group of cards" className="focus:outline-none px-4 lg:px-0">
+                    <div className="mx-auto container flex flex-wrap px-2 lg:px-24">
+                        <div tabIndex={0} aria-label="card 1" className="focus:outline-none flex sm:w-full md:w-1/2 lg:border-r md:border-r sm:border-r-0 border-orange-400 pb-10 lg:pt-10">
+                            <div className=" flex flex-shrink-0 mr-5">
+                                <img src="https://tuk-cdn.s3.amazonaws.com/can-uploader/orange_bg_with_divider-svg1.svg" alt="drawer" />
+                            </div>
+                            <div className="md:w-9/12 lg:w-9/12">
+                                <h2 tabIndex={0} className="focus:outline-none text-lg font-semibold leading-5">Streamline Operations</h2>
+                                <p tabIndex={0} className="focus:outline-none text-base leading-normal xl:w-10/12 pt-2">The website should aim to streamline logistics processes, such as inventory management, order tracking, and delivery scheduling. By centralizing and automating these tasks, the goal is to reduce errors, improve efficiency, and save time and resources.</p>
+                            </div>
+                        </div>
+                        <div tabIndex={0} aria-label="card 2" className="focus:outline-none flex sm:w-full md:w-1/2 lg:pb-10 lg:pt-10">
+                            <div className=" flex flex-shrink-0 sm:ml-0 md:ml-10 lg:ml-10 mr-5">
+                                <img src="https://tuk-cdn.s3.amazonaws.com/can-uploader/orange_bg_with_divider-svg2.svg" alt="check" />
+                            </div>
+                            <div className="md:w-9/12 lg:w-9/12 ">
+                                <h2 tabIndex={0} className="focus:outline-none text-lg font-semibold leading-5">Enhance Visibility</h2>
+                                <p tabIndex={0} className="focus:outline-none text-base leading-normal xl:w-10/12 pt-2">The website should provide real-time visibility into the logistics process, allowing users to track shipments, monitor inventory levels, and receive notifications and updates. The goal is to provide transparency and enable stakeholders to make informed decisions based on accurate and up-to-date information.</p>
+                            </div>
+                        </div>
+                        <div tabIndex={0} aria-label="card 3" className="focus:outline-none flex sm:w-full md:w-1/2 lg:border-t md:border-t sm:border-t-0 lg:border-r md:border-r sm:border-r-0 border-orange-400 pt-10 lg:pb-20">
+                            <div className=" flex flex-shrink-0 mr-5">
+                                <img src="https://tuk-cdn.s3.amazonaws.com/can-uploader/orange_bg_with_divider-svg3.svg" alt="html-tag" />
+                            </div>
+                            <div className="md:w-9/12 lg:w-9/12 ">
+                                <h2 tabIndex={0} className="focus:outline-none text-lg font-semibold leading-5">Improve Customer Experience</h2>
+                                <p tabIndex={0} className="focus:outline-none text-base leading-normal xl:w-10/12 pt-2"> The website should prioritize the customer experience by offering features such as easy online ordering, clear shipment tracking, and responsive customer support. The goal is to provide a seamless and satisfying experience for customers, leading to increased customer satisfaction and loyalty.</p>
+                            </div>
+                        </div>
+                        <div tabIndex={0} aria-label="card 4" className="focus:outline-none flex sm:w-full md:w-1/2 lg:border-t md:border-t sm:border-t-0 border-orange-400 pt-10 lg:pb-20">
+                            <div className=" flex flex-shrink-0 sm:ml-0 md:ml-10 lg:ml-10 mr-5">
+                                <img src="https://tuk-cdn.s3.amazonaws.com/can-uploader/orange_bg_with_divider-svg4.svg" alt="display" />
+                            </div>
+                            <div className="md:w-9/12 lg:w-9/12 ">
+                                <h2 tabIndex={0} className="focus:outline-none text-lg font-semibold leading-5">Optimize Supply Chain</h2>
+                                <p tabIndex={0} className="focus:outline-none text-base leading-normal xl:w-10/12 pt-2">The website should enable effective management of the supply chain, including inventory control, warehouse management, and transportation planning. The goal is to optimize the flow of goods, reduce costs, minimize stockouts, and improve overall supply chain performance.</p>
+                            </div>
+                        </div>
+                        <div tabIndex={0} aria-label="card 3" className="focus:outline-none flex sm:w-full md:w-1/2 lg:border-t md:border-t sm:border-t-0 lg:border-r md:border-r sm:border-r-0 border-orange-400 pt-10 lg:pb-20">
+                            <div className=" flex flex-shrink-0 mr-5">
+                                <img src="https://tuk-cdn.s3.amazonaws.com/can-uploader/orange_bg_with_divider-svg3.svg" alt="html-tag" />
+                            </div>
+                            <div className="md:w-9/12 lg:w-9/12 ">
+                                <h2 tabIndex={0} className="focus:outline-none text-lg font-semibold leading-5">Foster Collaboration</h2>
+                                <p tabIndex={0} className="focus:outline-none text-base leading-normal xl:w-10/12 pt-2">The website should facilitate collaboration between various stakeholders, such as suppliers, manufacturers, distributors, and customers. The goal is to enable seamless communication, data sharing, and coordination to enhance overall logistics efficiency and effectiveness.</p>
+                            </div>
+                        </div>
+                        <div tabIndex={0} aria-label="card 4" className="focus:outline-none flex sm:w-full md:w-1/2 lg:border-t md:border-t sm:border-t-0 border-orange-400 pt-10 lg:pb-20">
+                            <div className=" flex flex-shrink-0 sm:ml-0 md:ml-10 lg:ml-10 mr-5">
+                                <img src="https://tuk-cdn.s3.amazonaws.com/can-uploader/orange_bg_with_divider-svg4.svg" alt="display" />
+                            </div>
+                            <div className="md:w-9/12 lg:w-9/12 ">
+                                <h2 tabIndex={0} className="focus:outline-none text-lg font-semibold leading-5">Provide Analytics and Insights</h2>
+                                <p tabIndex={0} className="focus:outline-none text-base leading-normal xl:w-10/12 pt-2">The website should incorporate data analytics capabilities to generate meaningful insights and reports. The goal is to empower decision-makers with actionable information, enabling them to identify trends, make data-driven decisions, and continuously improve logistics operations.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
 
         </div>
     )
--- src/pages/Features/Features.jsx
+++ src/pages/Features/Features.jsx
@@ -200,6 +200,67 @@ const Features = () => {
                         </svg>
                     </a>
                 </div>
+
+                <div class="p-8 space-y-3 border-2 border-orange-400 dark:border-orange-300 rounded-xl">
+                    <span class="inline-block text-orange-500 dark:text-orange-400">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="w-8 h-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.657 18.657A8 8 0 016.343 7.343S7 9 9 10c0-2 .5-5 2.986-7C14 5 16.09 5.777 17.656 7.343A7.975 7.975 0 0120 13a7.975 7.975 0 01-2.343 5.657z" />
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9.879 16.121A3 3 0 1012.015 11L11 14H9c0 .768.293 1.536.879 2.121z" />
+                        </svg>
+                    </span>
+
+                    <h1 class="text-xl font-semibold text-gray-700 capitalize dark:text-white">Automated Notifications</h1>
+
+                    <p class="text-gray-500 dark:text-gray-300">
+                        Users receive automated notifications and updates regarding shipment status, order confirmations, and delivery alerts.
+                    </p>
+
+                    <a href="#" class="inline-flex p-2 text-orange-500 capitalize transition-colors duration-300 transform bg-orange-100 rounded-full rtl:-scale-x-100 dark:bg-orange-500 dark:text-white hover:underline hover:text-orange-600 dark:hover:text-orange-500">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                        </svg>
+                    </a>
+                </div>
+
+                <div class="p-8 space-y-3 border-2 border-orange-400 dark:border-orange-300 rounded-xl">
+                    <span class="inline-block text-orange-500 dark:text-orange-400">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="w-8 h-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 4a2 2 0 114 0v1a1 1 0 001 1h3a1 1 0 011 1v3a1 1 0 01-1 1h-1a2 2 0 100 4h1a1 1 0 011 1v3a1 1 0 01-1 1h-3a1 1 0 01-1-1v-1a2 2 0 10-4 0v1a1 1 0 01-1 1H7a1 1 0 01-1-1v-3a1 1 0 00-1-1H4a2 2 0 110-4h1a1 1 0 001-1V7a1 1 0 011-1h3a1 1 0 001-1V4z" />
+                        </svg>
+                    </span>
+
+                    <h1 class="text-xl font-semibold text-gray-700 capitalize dark:text-white">Returns and Reverse Logistics</h1>
+
+                    <p class="text-gray-500 dark:text-gray-300">
+                        The website facilitates the management of returns and reverse logistics processes, including return authorizations, tracking, and refunds.
+                    </p>
+
+                    <a href="#" class="inline-flex p-2 text-orange-500 capitalize transition-colors duration-300 transform bg-orange-100 rounded-full rtl:-scale-x-100 dark:bg-orange-500 dark:text-white hover:underline hover:text-orange-600 dark:hover:text-orange-500">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                        </svg>
+                    </a>
+                </div>
+
+                <div class="p-8 space-y-3 border-2 border-orange-400 dark:border-orange-300 rounded-xl">
+                    <span class="inline-block text-orange-500 dark:text-orange-400">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="w-8 h-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 3v4M3 5h4M6 17v4m-2-2h4m5-16l2.286 6.857L21 12l-5.714 2.143L13 21l-2.286-6.857L5 12l5.714-2.143L13 3z" />
+                        </svg>
+                    </span>
+
+                    <h1 class="text-xl font-semibold text-gray-700 capitalize dark:text-white">Security and Data Protection</h1>
+
+                    <p class="text-gray-500 dark:text-gray-300">
+                        Logistics websites prioritize the security of user data and employ measures such as encryption, secure authentication, and data backup to ensure the protection of sensitive information.
+                    </p>
+
+                    <a href="#" class="inline-flex p-2 text-orange-500 capitalize transition-colors duration-300 transform bg-orange-100 rounded-full rtl:-scale-x-100 dark:bg-orange-500 dark:text-white hover:underline hover:text-orange-600 dark:hover:text-orange-500">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                        </svg>
+                    </a>
+                </div>
             </div>
 
         </div>
