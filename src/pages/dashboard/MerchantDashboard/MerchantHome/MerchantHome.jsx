69f32a13a3baa53482c92d3f8e6afdbddbd1ffcb
Abu Taher <writerabutaher@gmail.com>
2023-06-06 20:45:21 +0600

    Announcement

--- src/pages/dashboard/MerchantDashboard/MerchantHome/MerchantHome.jsx
+++ src/pages/dashboard/MerchantDashboard/MerchantHome/MerchantHome.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
-import MerchantStats from './MerchantStats'
+import MerchantHeader from './MerchantHeader'
+import OrderSummary from './OrderSummary'
+import PaymentSummary from './PaymentSummary'
+import Announcement from './Announcement'
 
 const MerchantHome = () => {
     return (
-        <div>
-            <MerchantStats />
+        <div className='max-w-screen-2xl mx-auto px-4'>
+            <MerchantHeader />
+            <Announcement />
+            <OrderSummary />
+            <PaymentSummary />
         </div>
     )
 }
