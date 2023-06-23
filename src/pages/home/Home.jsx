d1eccbced0a5290cf2acee657eaed767f5434373
Abu Taher <writerabutaher@gmail.com>
2023-06-23 13:05:00 +0600

    section 2

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,7 @@
 import { useEffect } from "react";
 import About from "./about/About";
-import Banner from "./banner/Banner";
 import Faq from "./faq/Faq";
 import Feature from "./feature/Feature";
-import Pricing from "./pricing/Pricing";
 import Review from "./review/Review";
 import Service from "./service/Service";
 import Hero3 from "./hero/hero";
@@ -18,12 +16,10 @@ const Home = () => {
     return (
         <div>
             <Hero3 />
-            {/* <Banner /> */}
             <Service />
             <Section1 />
             <Feature />
             <Section2 />
-            <Pricing />
             <Review />
             <Faq />
             <About />
